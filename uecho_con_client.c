e7ce9f43f39179bc229992cffc770216a92a7d8f
YOUNGJU <clleo97@naver.com>
2020-04-19 17:00:46 +0900

    Week5_Connected_UDP

--- uecho_con_client.c
+++ uecho_con_client.c
@@ -31,8 +31,9 @@ int main(int argc, char* argv[])
 	serv_adr.sin_addr.s_addr = inet_addr(argv[1]);
 	serv_adr.sin_port = htons(atoi(argv[2]));
 
+	// sock에 serv_adr(목적지 주소 정보)를 등록
+	// 연결 요청을 보내는 것이 아니라 목적지 주소 정보를 등록하기 위해 connect 사용(TCP와의 차이점)
 	connect(sock, (struct sockaddr*) & serv_adr, sizeof(serv_adr));
-
 	while (1)
 	{
 		fputs("Insert message(q to quit): ", stdout);
@@ -42,17 +43,19 @@ int main(int argc, char* argv[])
 		if (!strcmp(message, "q\n") || !strcmp(message, "Q\n"))
 			break;
 
+		// connect를 하지 않았다면 목적지 주소 정보를 계속 등록해야함(1단계, 3단계 등록을 안해도됨 - 절약 가능)
 		/*	
 		sendto(sock, message, strlen(message), 0,
 			(struct sockaddr*) & serv_adr, sizeof(serv_adr));
 		*/
 		write(sock, message, strlen(message));
+
+		// connect를 사용하였기 때문에 발신지 정보를 담은 구조체를 넘겨주지 않아도됨
 		/*
 		adr_sz = sizeof(from_adr);
 		str_len = recvfrom(sock, message, BUF_SIZE, 0,
 			(struct sockaddr*) & from_adr, &adr_sz);
 		*/
-
 		str_len = read(sock, message, sizeof(message) - 1);
 
 		message[str_len] = 0;
