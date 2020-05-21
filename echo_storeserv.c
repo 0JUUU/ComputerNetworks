c51b7cddb82eabe5a695f04455b8b063474c98b5
YOUNGJU <clleo97@naver.com>
2020-05-21 12:12:09 +0900

    Week10_echo_store_message_NOTYET

---
+++ echo_storeserv.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 100
+void error_handling(char* message);
+void read_childproc(int sig);
+
+// 메세지를 저장하는 형태의 에코 서버
+int main(int argc, char* argv[])
+{
+	int serv_sock, clnt_sock;
+	struct sockaddr_in serv_adr, clnt_adr;
+
+	// pipe의 파일 디스크립터가 저장될 배열 선언
+	int fds[2];
+
+	pid_t pid;
+	struct sigaction act;
+	socklen_t adr_sz;
+	int str_len, state;
+	char buf[BUF_SIZE];
+	if (argc != 2)
+	{
+		printf("Usage : %s <port> \n", argv[0]);
+		exit(1);
+	}
+
+	act.sa_handler = read_childproc;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	state = sigaction(SIGCHLD, &act, 0);
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_adr.sin_port = htons(atoi(argv[1]));
+
+	if (bind(serv_sock, (struct sockaddr*) & serv_adr, sizeof(serv_adr)) == -1)
+		error_handling("bind() error");
+	if (listen(serv_sock, 5) == -1)
+		error_handling("listen() error");
+
+	pipe(fds);
+	pid = fork();
+
+	// 서버에서 처음으로 생성하는 자식 프로세스
+	if (pid == 0)
+	{
+		FILE* fp = fopen("echomsg.txt", "wt");
+		char msgbuf[BUF_SIZE];
+		int i, len;
+
+		for (i = 0; i < 10; i++) 
+		{
+			len = read(fds[0], msgbuf, BUF_SIZE);
+			fwrite((void*)msgbuf, 1, len, fp);
+		}
+		fclose(fp);
+		return 0;
+	}
+
+	while (1)
+	{
+		adr_sz = sizeof(clnt_adr);
+		clnt_sock = accept(serv_sock, (struct sockaddr*) & clnt_adr, &adr_sz);
+		if (clnt_sock == -1)
+			continue;
+		else
+			puts("nes client connected...");
+
+		// 서버에서 연결 허용 시마다 생성하는 자식 프로세스
+		pid = fork();
+		if (pid == 0)
+		{
+			close(serv_sock);
+			// 첫번째 자식 프로세스가 읽게 되는 데이터
+			while ((str_len = read(clnt_sock, buf, BUF_SIZE)) != 0)
+			{
+				write(clnt_sock, buf, str_len);
+				write(fds[1], buf, str_len);
+			}
+
+			close(clnt_sock);
+			puts("client disconnected...");
+			return 0;
+		}
+		else
+			close(clnt_sock);
+	}
+
+	close(serv_sock);
+	return 0;
+}
+
+void read_childproc(int sig)
+{
+	pid_t pid;
+	int status;
+	// blocking 상태가 되지 않도록
+	pid = waitpid(-1, &status, WNOHANG);
+	printf("Removed proc id: %d \n", pid);
+
+}
+
+void error_handling(char* message)
+{
+	fputs(message, stderr);
+	fputc('\n', stderr);
+	exit(1);
+}
